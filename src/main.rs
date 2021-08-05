1bbf0bb5856d7da74b6d71988d9fe86f1990a9ea
RaphaelKreft <r.kreft@unibas.ch>
2021-08-05 18:38:10 +0200

    implemented cli-arg parsing with structopt; Adapted drivers of both evaluators(regular & non-det)

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,18 @@ use types::{RlErr, RlReturn, RlType};
 use std::error::Error;
 
 // standard library imports
-
+use structopt::StructOpt;
 
 #[macro_use]
 extern crate lazy_static;
-
 extern crate rustyline;
 
 /**
-    This function returns a vector of RLisp expressions, that define functions in RLisp itself.
-    Here Users can add definitions or commands that should be executed every time executing RLisp.
+This function returns a vector of RLisp expressions, that define functions in RLisp itself.
+Here Users can add definitions or commands that should be executed every time executing RLisp.
 
-    Returns: Vector of Strings that should contain RLisp expressions
-*/
+Returns: Vector of Strings that should contain RLisp expressions
+ */
 fn self_defined_prebuild() -> Vec<String> {
     vec![
         "(define caar (lambda (x) (car (car x))))".to_string(),
@@ -50,12 +49,22 @@ fn self_defined_prebuild() -> Vec<String> {
     ]
 }
 
+#[derive(StructOpt)]
+struct Cli {
+    /// Flag to tell if non-deterministic evaluator should be used
+    #[structopt(short = "n", long = "non-det", default_value = False, about = "if this flag is given, the non deterministic evaluator is used")]
+    non_det: bool,
+    /// The path to the file to read (optional)
+    #[structopt(parse(from_os_str), about = "if this argument is given, the interpreter tries to load from the given path")]
+    path: Option<std::path::PathBuf>,
+}
+
 /**
-    main() is the entry-point into RLisp, it checks for cmd arguments and if there is an
-    argument, try to load from file with this name, otherwise start REPL(Read-Eval-Print-Loop)
-*/
+main() is the entry-point into RLisp, it checks for cmd arguments and if there is an
+argument, try to load from file with this name, otherwise start REPL(Read-Eval-Print-Loop)
+ */
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args = Cli::from_args();
     // create a new global environment (stdlib already loaded)
     let env = env::init_global();
     // execute/evaluate self defined RLisp expressions
@@ -63,58 +72,63 @@ fn main() {
         rep_wrapper(definition, env.clone(), false);
     }
     // Operate on given arguments
-    if args.len() == 2 && &args[1] != "non-det" {
-        // if argument was given and is not "non-det" open and operate on file input
-        load(&args[1], env.clone());
-    } else if args.len() == 2 && &args[1] == "non-det" {
-        amb_driver_loop(env.clone(), true)
-    } else {
-        // else operate in cmd mode -> REPL
-        let mut rl = rustyline::Editor::<()>::new();
-        loop {
-            // use extern crate rustyline, to get userinput
-            let input = rl.readline("user> ");
-            // check if there was a valid input
-            match input {
-                // There was a valid input
-                Ok(line) => {
-                    // if command == exit, break out of the REPL
-                    if line == "exit" {
-                        break;
-                    }
-                    // if command != exit, call the rep-wrapper with the global environment
-                    rep_wrapper(&line, env.clone(), true);
-                }
-                // There was no valid input -> Give information and repeat the loop
-                Err(_) => println!("No input"),
+    match &args.path {
+        Some(t) => load(t, env.clone(), args.non_det),
+        _ => {
+            // else operate in cmd mode -> REPL
+            let mut rl = rustyline::Editor::<()>::new();
+            if !args.non_det {
+                normal_loop(rl, enc.clone());
+            } else {
+                amb_driver_loop(rl,env.clone());
             }
         }
     }
 }
-// TODO: Check if loop can be  changed so that also files can be evaluated with non-det Rlisp
+
+fn normal_loop(mut reader: rustyline::Editor<()>, env: RlEnv) {
+    loop {
+        // use extern crate rustyline, to get userinput
+        let input = reader.readline("user> ");
+        // check if there was a valid input
+        match input {
+            // There was a valid input
+            Ok(line) => {
+                // if command == exit, break out of the REPL
+                if line == "exit" {
+                    break;
+                }
+                // if command != exit, call the rep-wrapper with the global environment
+                rep_wrapper(&line, env.clone(), true);
+            }
+            // There was no valid input -> Give information and repeat the loop
+            Err(_) => println!("No input"),
+        }
+    }
+}
+
 /**
 The amb_driver_loop is the loop that is used to evaluate non-deterministic RLisp.
 In the current version it must be called explicitly by given appropriate arg to the executable.
 
-@args:  env  - the environment the expression is evaluated in
-**/
-fn amb_driver_loop(env: RlEnv) {
-    let rl = rustyline::Editor::<()>::new();
+Arguments:  env  - the environment the expression is evaluated in
+ **/
+fn amb_driver_loop(mut reader: rustyline::Editor<()>, env: RlEnv) {
     let loop_func = || {
         println!("No current problem!");
-        amb_driver_loop(env.clone());
+        amb_driver_loop(reader.clone(),env.clone());
     };
-    internal_loop(rl, env.clone(), loop_func)
+    non_det_loop(reader.clone(), env.clone(), loop_func)
 }
 
-fn internal_loop <F: Fn()>(mut rl: rustyline::Editor<()>, env: RlEnv, try_again: F){
+fn non_det_loop<F: Fn()>(mut rl: rustyline::Editor<()>, env: RlEnv, try_again: F) {
     let input = rl.readline("(ambeval) user> ");
     match input {
         Ok(line) => {
             // for user to quit evaluator
             if line == "exit" {
                 return;
-            // for user to try-again, just loops when there is no problem
+                // for user to try-again, just loops when there is no problem
             } else if line == "try-again" {
                 try_again();
             } else {
@@ -127,78 +141,73 @@ fn internal_loop <F: Fn()>(mut rl: rustyline::Editor<()>, env: RlEnv, try_again:
                 };
                 let root_fail = || {
                     println!("There are no more values left!");
-                    amb_driver_loop(env.clone(), true);
+                    amb_driver_loop(rl.clone(),env.clone());
                 };
-                //new amb_rep_wrapper?
-                //where we also pass the succes and failure continuations
-                // then we shouldn't need the call to internal_loop below anymore
-                // and internal loop above can be uncommented
-                rep_wrapper(&line, env.clone(),true);
-                internal_loop(rl, env.clone(), try_again)
+                amb_eval(READ(&line)?, env);
             }
         }
         Err(_) => {
             println!("No input");
-            internal_loop(rl, env.clone(), try_again);
+            non_det_loop(rl, env.clone(), try_again);
         }
     }
 }
 
 /**
-    Calls reader to convert a string to an AST(Abstract Syntax tree). That is a language specific
-    Data-structure that the Evaluator can work with internally.
+Calls reader to convert a string to an AST(Abstract Syntax tree). That is a language specific
+Data-structure that the Evaluator can work with internally.
 
-    Arguments:  plain_input - a String that should be parsed into an AST
-    Returns:    Of type RLReturn, so either a valid AST or an (Parsing)Error
-*/
+Arguments:  plain_input - a String that should be parsed into an AST
+Returns:    Of type RLReturn, so either a valid AST or an (Parsing)Error
+ */
 fn READ(plain_input: &String) -> RlReturn {
     return reader::read_str(plain_input.to_string());
 }
 
 /**
-    Takes an AST and an Environment, calls eval::eval(The Interface to the Evaluator-Part of the Interpreter)
-    and returns the result of the Evaluator.
+Takes an AST and an Environment, calls eval::eval(The Interface to the Evaluator-Part of the Interpreter)
+and returns the result of the Evaluator.
 
-    Arguments:  expression - the AST(type RLType) that should be evaluated
-                env - the environment that the evaluator should work with
-    Returns:    of type RlReturn, so either the result or an (Evaluation)Error
-*/
+Arguments:  expression - the AST(type RLType) that should be evaluated
+            env - the environment that the evaluator should work with
+Returns:    of type RlReturn, so either the result or an (Evaluation)Error
+ */
 fn EVAL(expression: RlType, env: RlEnv) -> RlReturn {
     return eval(expression, env);
 }
 
 /**
-    Takes an AST and calls printer::print_str to turn AST into String, then returns this String
+Takes an AST and calls printer::print_str to turn AST into String, then returns this String
 
-    Arguments:  text - the AST that should be converted to a string
-    Returns:    the String to the given text
-*/
+Arguments:  text - the AST that should be converted to a string
+Returns:    the String to the given text
+ */
 fn PRINT(text: RlType) -> String {
     return printer::print_str(text);
 }
 
 /**
-   read-eval-print: Chain calls READ-EVAL-PRINT
+read-eval-print: Chain calls READ-EVAL-PRINT
 
-   Arguments:   to_process - an input string that should be interpreted
-                env - the environment the expression/input string is evaluated in
-   Returns:     Error? -> object with type RLError is returned, otherwise the result sting
-*/
+Arguments:   to_process - an input string that should be interpreted
+             env - the environment the expression/input string is evaluated in
+Returns:     Error? -> object with type RLError is returned, otherwise the result sting
+ */
 fn rep(to_process: &String, env: RlEnv) -> Result<String, RlErr> {
     return Ok(PRINT(EVAL(READ(to_process)?, env)?));
 }
 
 /**
-    Interface to rep:
-    It calls rep with the string and the environment, print out results or errors nicely if print
-    flag is set.
+Interface to rep:
+It calls rep with the string and the environment, print out results or errors nicely if print
+flag is set.
 
-    Arguments:  to_rep - the string to process
-                env - the environment, that the Evaluator works with
-                print_flag - determines whether the errors or results are printed out
+Arguments:  to_rep - the string to process
+            env - the environment, that the Evaluator works with
+            print_flag - determines whether the errors or results are printed out
 
-    Returns:    -
-*/
+Returns:    -
+ */
 fn rep_wrapper(to_rep: &String, env: RlEnv, print_flag: bool) {
     // if there was an input
     if to_rep.len() > 0 {
@@ -219,28 +228,32 @@ fn rep_wrapper(to_rep: &String, env: RlEnv, print_flag: bool) {
 }
 
 /**
-    Takes a filename and reads the content as String, if an error occurs, NIL is returned
+Takes a filename and reads the content as String, if an error occurs, NIL is returned
 
-    Arguments:  filename - the name of the file to read as String
-    Returns:    file-content as String
-*/
+Arguments:  filename - the name of the file to read as String
+Returns:    file-content as String
+ */
 fn read_file_string(filename: String) -> String {
     return fs::read_to_string(filename).expect("#nil");
 }
 
 /**
-    Takes a filename and an environment uses read_file_string to read string from file, then
-    surrounds this string with a (do ) expression -> This is so that all expressions in the
-    file are executed even if there are multiple independent ones on multiple lines
-    (newlines are removed in the parsing process).
+Takes a filename and an environment uses read_file_string to read string from file, then
+surrounds this string with a (do ) expression -> This is so that all expressions in the
+file are executed even if there are multiple independent ones on multiple lines
+(newlines are removed in the parsing process).
 
-    Arguments:  filename - name of file to read from
-                env - the environment, the loaded expressions should be evaluated in
-*/
-fn load(filename: &String, env: RlEnv) {
+Arguments:  filename - name of file to read from
+            env - the environment, the loaded expressions should be evaluated in
+ */
+fn load(filename: &String, env: RlEnv, non_det: bool) {
     // load file string and pack into do expression
     let file_string = read_file_string(filename.to_string());
     let to_execute = format!("(do {})", file_string);
     // execute prepared expression
-    rep_wrapper(&to_execute, env, true)
+    if non_det {
+        // TODO: implement file read for non-det -> break out core func of amb-drivers
+    } else {
+        rep_wrapper(&to_execute, env, true);
+    }
 }
